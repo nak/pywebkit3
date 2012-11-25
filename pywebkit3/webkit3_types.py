afa66368eefb7fee906f7e211f28c615ee34ad13
John Rusnak <jrusnak@sensorplatforms.com>
2012-11-25 14:04:38 -0800

    Fixed issue with use of [] in interface files

--- pywebkit3/webkit3_types.py
+++ pywebkit3/webkit3_types.py
@@ -1,4 +1,8 @@
 from ctypes import *
 
-cdll.LoadLibrary("libwebkit2gtk-3.0.so")
-libwebkit3 = CDLL("libwebkit2gtk-3.0.so")
+try:
+    cdll.LoadLibrary("libwebkit2gtk-3.0.so")
+    libwebkit3 = CDLL("libwebkit2gtk-3.0.so")
+except:
+    cdll.LoadLibrary("libwebkitgtk-3.0.so.0")
+    libwebkit3 = CDLL("libwebkitgtk-3.0.so.0")
