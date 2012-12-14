8639efc22b90edf71a374422545324b1430c1c8f
John Rusnak <jrusnak@sensorplatforms.com>
2012-12-13 22:06:49 -0800

    bump version to 0.7.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup( name = "PyGGI",
-    version = "0.7.1",
+    version = "0.7.2",
     packages = ["pyggi"],# find_packages(),
     scripts = ["fmVisualization"],
 
