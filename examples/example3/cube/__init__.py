abe3b0e39f36cf9b751f37476044809d514ee702
John Rusnak <jrusnak@sensorplatforms>
2014-04-14 13:12:03 -0700

    Added 3rd example (moved from the simple "test" folder areas)

---
+++ examples/example3/cube/__init__.py
@@ -0,0 +1,59 @@
+from pyggi.javascript import *
+
+
+
+class YPR_Updater( JavascriptClass ):
+    """
+    This class is reponsible for updating the yaw pitch an roll
+    (orientation) of a cube, with the javascript calling the
+    update method and get methods.  By inheriting from JavascriptClass,
+    when this module is exported to javascript, it will automatically
+    become available to execute under javascript code!
+    """
+
+    
+    def __init__(self, context, name, *init_angles):
+        JavascriptClass.__init__(self, context, name )
+        self.ypr =  [angl for angl in init_angles]
+        self.sign = 1
+        
+
+    def __del__(self):
+        pass
+
+    index = 0
+    def update(self, offset_yaw, offset_pitch, offset_roll):
+        import logging
+        YPR_Updater.index += 1
+        # Now update the angles for yaw pitch roll for next iteration
+        self.ypr[0] += offset_yaw
+        self.ypr[1] += offset_pitch
+        self.ypr[2] += offset_roll
+        if(self.ypr[2]> 90 ):
+            self.sign=-self.sign;
+            
+            self.ypr[2] -= 180;
+            self.ypr[0] -= 180;
+            self.ypr[1] = 180-self.ypr[1];
+             
+
+        if(self.ypr[0]>=360):
+            self.ypr[0] -=360;
+        else:
+            self.ypr[0] += 360;
+    
+        if(self.ypr[1]>=180) :
+            self.ypr[1] -= 360;
+        else:
+            self.ypr[1] += 360;
+
+            
+
+    def yaw(self):
+        return self.ypr[0]
+
+    def pitch(self):
+        return self.ypr[1]
+
+    def roll(self):
+        return self.ypr[2]
---
+++ examples/example3/testit.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+import sys
+import os.path
+sys.path = [ os.path.abspath( os.path.join( os.path.dirname(__file__),".."))] + \
+           sys.path
+
+from pyggi import gtk3,webkit3,javascript, gobject
+
+####################
+# Ophidian is the python equivalent of javascript.  Any library in javascript
+# is easily exported to python.  For jQuery, the Python equivalent of the  "$"
+# operator is provided as the "_" operator in Python.
+#
+# NOTE: Any javascript or jQuery calls MUST be done only after a context is available,
+# usually implying a webview is "ready", determined via the
+# WebKitWebView class's on_view_ready callback.  Also, javascript calls will have no impact
+# unless run from the gtk main thread (this IS GTK).  
+######################
+
+## Use ophidian much like jQuery t set the colors on the cube
+## faces.
+from pyggi.javascript import jquery
+import logging
+
+count= 0
+def set_bg(color = 0 ):
+    # The callback for "each":
+    global jq
+    _ = jq
+    def change( index, obj):
+        global count
+        if index%3 == 0:
+            index = (index + count) %10
+            color = "#CC%d%d%d%d"%(index,index, index, index)
+        elif index%3 == 1:
+            index = (index + count) %10            
+            color = "#%d%dAA%d%d"%(index,index, index, index)
+        else:
+            index = (index + count) %10            
+            color = "#%d%d%d%d88"%(index,index, index, index)
+        count += 1
+        count = count %10
+        _(obj).css('background-color',color)
+
+        return True
+    #Run the callback on each cube face (cubie) a'la jQurey interface
+    #through ophdian:
+    _('.cubie').each(change)     
+    return True
+
+#create the webkit webview and load a local file URI
+web = webkit3.WebKitWebView()
+web.load_uri("file://%s/test.html"%os.path.abspath(os.path.dirname(__file__)))
+
+#create the containing window
+window = gtk3.GtkWindow(gtk3.GTK_WINDOW_TOPLEVEL)
+#close this app on close of the window:
+window.connect("delete-event", gtk3.main_quit)
+
+#when web view is read, mak a callback to set_bg,
+#which by the nature of it being a GTK callback,
+#will be executed in the main thread
+web.on_view_ready( set_bg, "red")
+gobject.timeout_add( 1000, set_bg)
+#add our web view to the main window, and set properties
+window.add(web)
+window.set_default_size( 250, 100)
+window.set_title("PyWebkit with CSS 3D ala Python")
+
+
+#get the context for the javascript environment:
+frame = web.get_main_frame()
+context =frame.get_global_context()
+#create a class available to javascript, The YPR_Updater class
+#is made available to javascript simply by inheriting from JavascriptClass
+#Javascript code can create a python YPR_Updater object view the
+#"cube.YPR_Updater.new_ method
+from cube import YPR_Updater
+YPR_Updater.web = web
+
+#now create all classes under cube by exporting that module:
+#We must provide an execution context (environment) which is done
+#via the get_env() call.  This will also initialize jQuery python
+#interface module if not already initialized
+import cube
+javascript.export_module(web.get_env(), cube)
+jq = jquery.initialize(web.get_env(), web.on_view_ready)
+
+
+#and show it!
+window.show_all()
+gtk3.main()
